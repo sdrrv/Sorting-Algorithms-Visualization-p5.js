469086c98b79a038a293367971cc3e3b3249e904
sdrrv <duartecelvas@gmail.com>
2021-09-22 20:19:00 +0100

    Full page overflow fixed

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+    noCursor();
 }
 
 function draw() {
     background(220);
-    circle(200, 200, 50);
-    rect(30, 20, 55, 55, 20);
+    fill(0);
+    circle(windowWidth / 2, windowHeight / 2, 50);
+    fill(color(250, 0, 0));
+    noStroke();
+    circle(mouseX, mouseY, 10);
+    //rect(30, 20, 55, 55, 20);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
