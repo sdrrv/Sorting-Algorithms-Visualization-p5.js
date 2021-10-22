bccb076df8482b45d964abea13b2082fd68373fa
sdrrv <duartecelvas@gmail.com>
2021-10-22 17:20:21 +0100

    Starting on the animations

--- sketch.js
+++ sketch.js
@@ -1,36 +1,40 @@
-let sortingAlgorithms = ["Selection Sort", "Buble Sort", "Quick Sort", "Merge Sort"]
+let sortingAlgorithms = ["Selection Sort", "Bubble Sort", "Quick Sort", "Merge Sort"]
+let sorting = false;
+let sortedList = [];
+
+let currentIndex = 0;
+let step = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // -----------Size-Slider----------------
-    sizeSlider = createSlider(10, 95, 25, 5);
+    sizeSlider = createSlider(10, 95, 50, 5);
     sizeSlider.position(windowWidth * 0.01, windowHeight * 0.04);
     sizeSlider.style('width', '100px');
     size = 25;
     numList = populateNumList(size, 100);
     // ------------Speed-Slider-------------------------
-    speedSlider = createSlider(0, 100, 50, 5);
+    speedSlider = createSlider(10, 100, 50, 10);
     speedSlider.position(windowWidth * 0.2, windowHeight * 0.04);
     speedSlider.style('width', '100px');
-    speed = 50;
     // --------------------------------------------------
-
-
-
-    chooseBar(windowWidth * 0.85, windowHeight * 0.02)
+    chooseBar(windowWidth * 0.85, windowHeight * 0.02) // choose algo
     noStroke();
     fill(250);
     sortButton = createButton("Sort!");
     sortButton.position(windowWidth * 0.95, windowHeight * 0.02);
-    sortButton.size(windowWidth / windowHeight * 25);
+    //sortButton.style('background-color', color(25, 23, 200, 50));
+    //sortButton.size(windowWidth / windowHeight * 25);
     sortButton.mousePressed(sortList);
-
-
-    print(speedSlider.x)
+    // ---------------------------------------------------
+    reShuffleButton = createButton("ReShuffle");
+    reShuffleButton.position(windowWidth * 0.78, windowHeight * 0.02);
+    reShuffleButton.mousePressed(reShuffle);
 }
 
 function draw() {
     background(220);
+    stroke(0);
     //----Array-Size---------
     strokeWeight(1);
     textSize(15);
@@ -55,35 +59,53 @@ function draw() {
 
     if (sizeSlider.value() != size) {
         size = sizeSlider.value();
-        numList = populateNumList(size, 100);
+        reShuffle();
     }
 
-    drawNumList();
+    if (sorting) {
+        if (currentIndex >= size - 1) {
+            step = 0;
+            sorting = false;
+            //reShuffle();
+        } else if (step <= 0) {
+            numList = sortList[currentIndex];
+            step = (110 - sizeSlider.value());
+        } else {
+            step += 1;
+        }
+        drawNumList();
+    } else {
+        drawNumList();
+    }
 }
 
 
-function populateNumList(listSize, maxNum) {
+function populateNumList(listSize, maxNum) { // Remake a list
     let resList = [];
     for (var i = 0; i < listSize; i++) {
-        resList[i] = random(1, maxNum);
+        resList[i] = [];
+        resList[i][0] = random(1, maxNum);
+        resList[i][1] = color(0, 0, 0);
     }
     return resList;
 }
 
-function drawNumList() {
+function drawNumList() { // Draw the list
 
     let currentPosX = windowWidth * (0.39 - (0.38 / 70) * (size - 25));
     let currentPosY = windowHeight * 0.98;
     for (var i = 0; i < size; i++) {
         strokeWeight(10);
-        stroke(0);
-        line(currentPosX, currentPosY, currentPosX, currentPosY - 2 * numList[i]);
+        stroke(numList[i][1]);
+        line(currentPosX, currentPosY, currentPosX, currentPosY - 2 * numList[i][0]);
         currentPosX += 15;
     }
 }
 
+
 function sortList() {
-    numList = selectionSort(numList);
+    sortedList = selectionSort(numList);
+    sorting = true;
 }
 
 function chooseBar(X, Y) {
@@ -98,7 +120,11 @@ function chooseBar(X, Y) {
     //sel.changed(mySelectEvent);
 }
 
-function selectionSort(inputArr) {
+function reShuffle() {
+    numList = populateNumList(size, 100);
+}
+
+function selectionSort(inputArr) { // 
     let n = inputArr.length;
 
     for (let i = 0; i < n; i++) {
